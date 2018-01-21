732aa8a40d54aaac8538436852ca160b1e3130e0
Ashley Kitson <ashley.kitson@freetimers.com>
2018-01-21 21:36:45 +0000

    add output to createdb.sh for debugging

--- createdb.sh
+++ createdb.sh
@@ -9,6 +9,9 @@ DBNAME=$1
 DBUID=$2
 DBPWD=$3
 
+echo "building tables"
 cat ./src/sql/build-tables.sql | mysql -u $DBUID -p$DBPWD $DBNAME
+echo "building procs and functions"
 cat ./src/sql/build-procs.sql | mysql -u $DBUID -p$DBPWD $DBNAME
+echo "building triggers"
 cat ./src/sql/build-triggers.sql | mysql -u $DBUID -p$DBPWD $DBNAME
