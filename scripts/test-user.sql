4b7766214a6ca06e95046939c50347574b26c3ee
Ashley Kitson <ashley.kitson@freetimers.com>
2018-01-22 06:48:09 +0000

    change travis build scripts

--- scripts/test-user.sql
+++ scripts/test-user.sql
@@ -2,5 +2,5 @@ use mysql;
 create database if not exists test;
 create user 'test'@'localhost' identified by 'test';
 # grant all PRIVILEGES on test.* to 'test'@'localhost';
-grant SUPER on test.* to 'test'@'localhost';
+grant SUPER on *.* to 'test'@'localhost';
 flush privileges;
