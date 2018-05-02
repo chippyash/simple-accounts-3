e4b5087e8abc9e0f855ca6f8ee6364a51ccae5cf
Ashley Kitson <ashley.kitson@freetimers.com>
2018-05-02 17:58:23 +0100

    script modifications

--- scripts/travis.install.mariadb.sh
+++ scripts/travis.install.mariadb.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
-#sudo service mysql stop
-#sudo apt-get install -y -o Dpkg::Options::='--force-confnew' mariadb-server mariadb-server-10.0 mariadb-oqgraph-engine-10.0
 sudo apt-get install -y mariadb-oqgraph-engine-10.0
-#sudo service mysql start
 sudo mysql < ./scripts/oqgraph.sql
 sudo mysql < ./scripts/test-user.sql
 ./createmariadb.sh test test test
