df34f440e4b88585017e90abfe689b252c691fcc
Ashley Kitson <ashley.kitson@zincdigital.com>
2020-10-19 09:03:15 +0100

    fixing mariadab oqgraph for travis

--- scripts/travis.install.mariadb.sh
+++ scripts/travis.install.mariadb.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sudo apt-get install -y mariadb-oqgraph-engine-10.0
+sudo apt-get install -y mariadb-plugin-oqgraph
 sudo mysql < ./scripts/oqgraph.sql
 sudo mysql < ./scripts/test-user.sql
 ./createmariadb.sh test test test
